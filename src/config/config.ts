36d65001e5c80fc6218a09cae428d2d2ee1a7ef9
NikShamin <o-l-y-m-p-i-c@mail.ru>
2025-10-23 23:22:37 +0300

    update

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,10 @@
 import { getBackendURL } from '../helpers/urlHelper';
 
 const config = {
-  TWITTER_API_URL: process.env.NEXT_PUBLIC_TWITTER_API_URL,
-  CORE_API_URL: getBackendURL('http'),
-  APP_NAME: 'TrenchSpy',
+    TWITTER_API_URL: process.env.NEXT_PUBLIC_TWITTER_API_URL,
+    CORE_API_URL: getBackendURL('http'),
+    APP_NAME: 'TrenchSpy',
+    APP_URL: process.env.NEXT_PUBLIC_APP_URL || 'https://trenchspy.com'
 };
 
 export { config };
