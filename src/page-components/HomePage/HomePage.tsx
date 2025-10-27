917ebece93880cbe66d1de4fc60cca283263d823
NikShamin <o-l-y-m-p-i-c@mail.ru>
2025-10-27 15:23:36 +0300

    delete suspense

--- src/page-components/HomePage/HomePage.tsx
+++ src/page-components/HomePage/HomePage.tsx
@@ -1,14 +1,11 @@
-import { Suspense } from 'react';
 import { HomePageSearchForm } from '@/components/HomePageSearchForm';
 
 export const HomePage = () => {
-  return (
-    <>
-      <main>
-        <Suspense fallback={<div>Loading...</div>}>
-          <HomePageSearchForm />
-        </Suspense>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main>
+                <HomePageSearchForm />
+            </main>
+        </>
+    );
 };
